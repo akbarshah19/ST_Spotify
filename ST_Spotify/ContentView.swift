0c376f1f2c3810d585b6bedac61d6b3b6860ff62
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-04-07 15:05:37 +0500

    Initial Commit

---
+++ ST_Spotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ST_Spotify
+//
+//  Created by Akbarshah Jumanazarov on 4/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ST_Spotify/ST_SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ST_SpotifyApp.swift
+//  ST_Spotify
+//
+//  Created by Akbarshah Jumanazarov on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ST_SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
